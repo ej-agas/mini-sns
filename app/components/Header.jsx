1a000aebd6114570b7809840536a2cae52a6c238
ej-agas <agas.ej09@gmail.com>
2022-06-06 22:15:05 +0800

    Add placeholder home component when user is logged in
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(
-    Boolean(localStorage.getItem("mini_sns_token"))
-  );
-
+function Header({ loggedIn, setLoggedIn }) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
--- app/main.jsx
+++ app/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,13 +7,18 @@ import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import Home from "./components/Home";
 
 const Main = () => {
+  const [loggedIn, setLoggedIn] = useState(
+    Boolean(localStorage.getItem("mini_sns_token"))
+  );
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<HomeGuest />} />
+        <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
